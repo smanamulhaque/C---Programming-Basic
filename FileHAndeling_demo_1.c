5375afe98765b5998afb3cc47d3afba62e79eb76
SHEIKH MD ANAMUL HAQUE <162632201+smanamulhaque@users.noreply.github.com>
2025-11-15 16:42:01 +0600

    Update FileHAndeling_demo_1.c

--- FileHAndeling_demo_1.c
+++ FileHAndeling_demo_1.c
@@ -1,13 +1,1 @@
-//Introduction to File
-#include<stdio.h>
-int main(){
-File *file;
-file=fopen("test.txt","W");
-if(file==NULL){
-    printf("File doesn't exist");
-}
-else{
-    printf("File is opened");
-    fclose(file);
-    getch(); 
-}
+
